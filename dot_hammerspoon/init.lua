21765ba522337fb91276a1bdc81bdc322b2e1e8a
pengzhendong <zhendong.peng@qq.com>
2025-03-28 11:00:32 +0800

    chang prefix from alt to ctrl

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -5,13 +5,13 @@ hs.window.animationDuration = 0
 -- Reload configuration
 hs.hotkey.bind({"cmd", "shift", "ctrl"}, "R", function() hs.reload() end)
 -- Register lock screen
-hs.hotkey.bind("alt", "L", function() hs.caffeinate.lockScreen() end)
+hs.hotkey.bind("ctrl", "L", function() hs.caffeinate.lockScreen() end)
 -- Register windowHints
-hs.hotkey.bind("alt", "Tab", function() hs.hints.windowHints() end)
+hs.hotkey.bind("ctrl", "Tab", function() hs.hints.windowHints() end)
 -- Register Hammerspoon console
-hs.hotkey.bind("alt", "Z", function() hs.toggleConsole() end)
+hs.hotkey.bind("ctrl", "Z", function() hs.toggleConsole() end)
 -- Register Hammerspoon API manual: Open Hammerspoon manual in default browser
-hs.hotkey.bind("alt", "H", function() hs.doc.hsdocs.forceExternalBrowser(true) hs.doc.hsdocs.moduleEntitiesInSidebar(true) hs.doc.hsdocs.help() end)
+hs.hotkey.bind("ctrl", "H", function() hs.doc.hsdocs.forceExternalBrowser(true) hs.doc.hsdocs.moduleEntitiesInSidebar(true) hs.doc.hsdocs.help() end)
 
 -- Use SpoonInstall Spoon to install other Spoons
 hs.loadSpoon("SpoonInstall")
@@ -20,11 +20,6 @@ Install = spoon.SpoonInstall
 Install:andUse("ModalMgr")
 Install:andUse("HCalendar")
 Install:andUse("HoldToQuit", { start = true })
-
---------------------------------------------------------------------------------
-Install:andUse("AClock", { fn = function(s)
-    hs.hotkey.bind("alt", "T", function() s:toggleShow() end)
-end})
 
 --------------------------------------------------------------------------------
 Install:andUse("Caffeine")
@@ -41,7 +36,7 @@ end
 --------------------------------------------------------------------------------
 -- 用于查看当前 App 快捷键
 Install:andUse("KSheet", { fn = function(s)
-    hs.hotkey.bind("alt", "S", function() spoon.KSheet:show() spoon.ModalMgr:deactivateAll() spoon.ModalMgr:activate({"cheatsheetM"}) end)
+    hs.hotkey.bind("ctrl", "S", function() spoon.KSheet:show() spoon.ModalMgr:deactivateAll() spoon.ModalMgr:activate({"cheatsheetM"}) end)
 end})
 if spoon.KSheet then
     spoon.ModalMgr:new("cheatsheetM")
@@ -51,43 +46,8 @@ if spoon.KSheet then
 end
 
 --------------------------------------------------------------------------------
-Install:andUse("ClipShow", { fn = function(s)
-    hs.hotkey.bind("alt", "C", function() spoon.ClipShow:toggleShow() if spoon.ClipShow.canvas:isShowing() then spoon.ModalMgr:deactivateAll() spoon.ModalMgr:activate({"clipshowM"}) end end)
-end})
-if spoon.ClipShow then
-    spoon.ModalMgr:new("clipshowM")
-    local cmodal = spoon.ModalMgr.modal_list["clipshowM"]
-    cmodal:bind('', 'escape', '退出', function() spoon.ClipShow:toggleShow() spoon.ModalMgr:deactivate({"clipshowM"}) end)
-    cmodal:bind('', 'Q', '退出', function() spoon.ClipShow:toggleShow() spoon.ModalMgr:deactivate({"clipshowM"}) end)
-    cmodal:bind('', 'N', '保存此会话', function() spoon.ClipShow:saveToSession() end)
-    cmodal:bind('', 'R', '恢复上一个会话', function() spoon.ClipShow:restoreLastSession() end)
-    cmodal:bind('', 'B', '在浏览器中打开', function() spoon.ClipShow:openInBrowserWithRef() spoon.ClipShow:toggleShow() spoon.ModalMgr:deactivate({"clipshowM"}) end)
-    cmodal:bind('', 'F', '保存到桌面', function() spoon.ClipShow:saveToFile() spoon.ClipShow:toggleShow() spoon.ModalMgr:deactivate({"clipshowM"}) end)
-    cmodal:bind('', 'H', 'Github 搜索', function() spoon.ClipShow:openInBrowserWithRef("https://github.com/search?q=") spoon.ClipShow:toggleShow() spoon.ModalMgr:deactivate({"clipshowM"}) end)
-    cmodal:bind('', 'S', '百度搜索', function() spoon.ClipShow:openInBrowserWithRef("https://www.baidu.com/search?wd=") spoon.ClipShow:toggleShow() spoon.ModalMgr:deactivate({"clipshowM"}) end)
-    cmodal:bind('', 'G', 'Google 搜索', function() spoon.ClipShow:openInBrowserWithRef("https://www.google.com/search?q=") spoon.ClipShow:toggleShow() spoon.ModalMgr:deactivate({"clipshowM"}) end)
-end
-
---------------------------------------------------------------------------------
-Install:andUse("CountDown", { fn = function(s)
-    hs.hotkey.bind("alt", "I", function() spoon.ModalMgr:deactivateAll() spoon.ModalMgr:activate({"countdownM"}, "#FF6347", true) end)
-end})
-if spoon.CountDown then
-    spoon.ModalMgr:new("countdownM")
-    local cmodal = spoon.ModalMgr.modal_list["countdownM"]
-    cmodal:bind('', 'escape', '退出面板', function() spoon.ModalMgr:deactivate({"countdownM"}) end)
-    cmodal:bind('', 'Q', '退出面板', function() spoon.ModalMgr:deactivate({"countdownM"}) end)
-    cmodal:bind('', '0', '5 分钟倒计时', function() spoon.CountDown:startFor(5) spoon.ModalMgr:deactivate({"countdownM"}) end)
-    for i = 1, 9 do
-        cmodal:bind('', tostring(i), string.format("%s 分钟倒计时", 10 * i), function() spoon.CountDown:startFor(10 * i) spoon.ModalMgr:deactivate({"countdownM"}) end)
-    end
-    cmodal:bind('', 'return', '25 分钟倒计时', function() spoon.CountDown:startFor(25) spoon.ModalMgr:deactivate({"countdownM"}) end)
-    cmodal:bind('', 'space', '暂停/恢复 倒计时', function() spoon.CountDown:pauseOrResume() spoon.ModalMgr:deactivate({"countdownM"}) end)
-end
-
---------------------------------------------------------------------------------
 Install:andUse("WinWin", { fn = function(s)
-    hs.hotkey.bind("alt", "R", function() spoon.ModalMgr:deactivateAll() spoon.ModalMgr:activate({"resizeM"}, "#B22222") end)
+    hs.hotkey.bind("ctrl", "T", function() spoon.ModalMgr:deactivateAll() spoon.ModalMgr:activate({"resizeM"}, "#B22222") end)
 end})
 if spoon.WinWin then
     spoon.ModalMgr:new("resizeM")
